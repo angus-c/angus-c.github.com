250d33041e7a9463e41bd2640496d338c3a58f26
angus croll <angus@twitter.com>
2014-10-20 22:08:55 -0700

    map integrated

--- hemingway/world/js/world.js
+++ hemingway/world/js/world.js
@@ -5,10 +5,6 @@
 
 d3.select(window).on("resize", throttle);
 
-// var zoom = d3.behavior.zoom()
-//     .scaleExtent([1, 8])
-//     .on("zoom", move);
-
 var width = document.getElementById('map').offsetWidth-60;
 var height = width / 2;
 
@@ -30,8 +26,7 @@ function setup(width,height){
       .attr("width", width)
       .attr("height", height)
       .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-      // .call(zoom);
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
   g = svg.append("g");
 
@@ -56,7 +51,7 @@ function draw(topo) {
       .attr("id", function(d,i) { return d.id; })
       .attr("title", function(d,i) { return d.properties.name; })
       .style("fill", function(d, i) {
-        return sites[d.properties.name] ? d.properties.color : "#AEAEAE"
+        return sites[d.properties.name] ? sites[d.properties.name].color : "#D1D2D1"
       });
 
   //ofsets plus width/height of transform, plsu 20 px of padding, plus 20 extra for tooltip offset off mouse
@@ -81,7 +76,7 @@ function draw(topo) {
       .on("click", function(d,i) {
         var country = d.properties.name;
         if (sites[country]) {
-          location.href = "http://www.amazon" + sites[country] + "/gp/product/1593275854/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1593275854&linkCode=as2&tag=ifhemwrojav-20&linkId=HEHXBBWQN7YSZSYD"
+          location.href = "http://www.amazon" + sites[country].domain + "/gp/product/1593275854/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1593275854&linkCode=as2&tag=ifhemwrojav-20&linkId=HEHXBBWQN7YSZSYD"
         }
       })
 
@@ -95,20 +90,6 @@ function redraw() {
   draw(topo);
 }
 
-function move() {
-
-  var t = d3.event.translate;
-  var s = d3.event.scale;
-  var h = height / 3;
-
-  t[0] = Math.min(width / 2 * (s - 1), Math.max(width / 2 * (1 - s), t[0]));
-  t[1] = Math.min(height / 2 * (s - 1) + h * s, Math.max(height / 2 * (1 - s) - h * s, t[1]));
-
-  zoom.translate(t);
-  g.style("stroke-width", 1 / s).attr("transform", "translate(" + t + ")scale(" + s + ")");
-
-}
-
 var throttleTimer;
 function throttle() {
   window.clearTimeout(throttleTimer);
@@ -118,14 +99,44 @@ function throttle() {
 }
 
 var sites  = {
-  'United States': '.com',
-  'United Kingdom': '.co.uk',
-  'France': '.fr',
-  'Japan': '.jp',
-  'Germany': '.de',
-  'India': '.in',
-  'China': '.cn',
-  'Italy': '.it',
-  'Canada': '.ca',
-  'Spain': '.es',
+  'United States': {
+    domain: '.com',
+    color: '#953B1F'
+  },
+  'United Kingdom': {
+    domain: '.co.uk',
+    color: '#E6729D'
+  },
+  'France': {
+    domain: '.fr',
+    color: '#193DBB'
+  },
+  'Japan': {
+    domain: '.jp',
+    color: '#72390C'
+  },
+  'Germany': {
+    domain: '.de',
+    color: '#7B1A64'
+  },
+  'India': {
+    domain: '.in',
+    color: '#F37615'
+  },
+  'China': {
+    domain: '.cn',
+    color: '#CC0000'
+  },
+  'Italy': {
+    domain: '.it',
+    color: '#4EB849'
+  },
+  'Canada': {
+    domain: '.ca',
+    color: '#8D95E1'
+  },
+  'Spain': {
+    domain: '.es',
+    color: '#FCD308'
+  }
 }
